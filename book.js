87f6a7f8244c5b0e6677eb7b73aff5f45528cf0b
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-01-17 16:30:24 -0500

    initial upload, add constructor function to JS Object exercise

---
+++ book.js
@@ -0,0 +1,16 @@
+//Task 1: Create a constructor function for the Book object with properties for title,
+ //author, and pages.
+
+
+//constructor function for Book
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+
+
+//Task 2: Implement a method within the Book object to display book information.
+
